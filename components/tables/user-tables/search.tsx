205319ef86ea73ff314a7b87d2c3647f4833bba7
guangnian-QinHai <5372604@qq.com>
2024-03-26 16:44:32 +0800

    [feat]add debounce for search

--- components/tables/user-tables/search.tsx
+++ components/tables/user-tables/search.tsx
@@ -2,11 +2,13 @@
 import { Input } from "@/components/ui/input";
 import { SearchProps } from "@/types/search";
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ queryParams }: SearchProps) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
-    const handleSearch = (param: string, value: string) => {
+
+    const handleSearch = useDebouncedCallback((param: string, value: string) => {
         const params = new URLSearchParams(searchParams);
         if (value) {
             params.set(param, value);
@@ -14,7 +16,9 @@ export default function Search({ queryParams }: SearchProps) {
             params.delete(param);
         }
         replace(`${pathname}?${params.toString()}`);
-    }
+    }, 300);
+
+
     return (
         <div className="flex flex-wrap gap-2">
             {queryParams.map(({ name, placeholder }) => (
